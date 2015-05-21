97e93e9c0d8ab1c6279fbae6c24090b6d6262fbe
jun <sv.junic@gmail.com>
2015-05-21 12:02:05 +0900

    Change promise to native code.

--- app.js
+++ app.js
@@ -4,26 +4,26 @@ var Iconv = require('iconv').Iconv;
 var conv = new Iconv('cp932','utf-8');
 var _ = require('lodash');
 
-var q = require('q');
-
 function dumpCsv( path ) {
-  var def = q.defer();
-  var json_array = [];
 
-  fs.readFile( path, function(err, sjisBuf) {
-    var buf = conv.convert(sjisBuf);
-    console.log( 'CSV Loading:' + path + ' *=-');
-    csv.parse(buf.toString(),{comment:'#'}, function(err, data) {
-      json_array.push( data );
-      def.resolve( json_array );
+  var promise = new Promise( function ( resolve, reject ) {
+    var json_array = [];
+
+    fs.readFile( path, function(err, sjisBuf) {
+      var buf = conv.convert(sjisBuf);
+      console.log( 'CSV Loading:' + path + ' *=-');
+      csv.parse(buf.toString(),{comment:'#'}, function(err, data) {
+        json_array.push( data );
+        resolve( json_array );
+      });
     });
-  });
+  })
 
-  return def.promise;
+  return promise;
 }
 
 dumpCsv( 'input.csv' )
-  .done( function ( data ) {
+  .then( function ( data ) {
     var dataValue = data[0];
     var dataName = dataValue.shift();
 
