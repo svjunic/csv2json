13654f3c1eaad0541155037fd5e9f524c5f83ba0
jun <sv.junic@gmail.com>
2015-05-22 14:07:27 +0900

    Fragmentation of the function.

--- csv2json.js
+++ csv2json.js
@@ -4,165 +4,169 @@ var Iconv = require('iconv').Iconv;
 var conv = new Iconv('cp932','utf-8');
 var _ = require('lodash');
 
+var csv2json = function () {
+  var app = {
+    debug         : false,
+    pretty        : false,
+    delimitter    : ':',
+    reg           : /:/,
+    outputJsonPath: '',
 
-/**
- * This module returns the CSV file in Json file or object.
- *
- * delimitter: ':' default
- *
- * @param  {String} input 読み込むファイルのパス
- * @param  {Option} option { outputJsonPath:'', delimitter:':', pretty:true }
- *       debug          : コンソールを出力します。
- *       outputJsonPath : 出力するJsonのパス。空文字や文字列でない場合は出力しない。
- *       delimitter     : CSVの区切り文字。１文字以上なければならない
- *       pretty         : Jsonの出力形式。trueをいれると圧縮された状態で出力。
- * @return {String} 出力したデータ内容
- */
-module.exports = function( input, option ){
+    setup: function ( option ) {
+    
+      if( _.isObject( option ) ) {
+        // debug
+        if( option.debug ) {
+          this.debug = true;
+        }
+    
+        // pretty
+        if( option.pretty ) {
+          this.pretty = true;
+        }
+    
+        // outputJsonPath
+        if( _.isString( option.outputJsonPath ) ) {
+          this.outputJsonPath = option.outputJsonPath;
+        }
+    
+        // delimitter
+        if( _.isObject( option ) && typeof _.isString( option.delimitter ) && option.delimitter !== '' ) {
+          this.delimitter = option.delimitter;
+        }
+      }
+    
+      // 区切り文字
+      if( _.isUndefined( this.delimitter ) ) {
+        this.delimitter = ':';
+      }
+    
+      this.reg = RegExp( this.delimitter );
+    },
 
-  var debug, pretty, delimitter, outputJsonPath;
-  var reg;
+    /**
+     * This module returns the CSV file in Json file or object.
+     *
+     * delimitter: ':' default
+     *
+     * @param  {String} input 読み込むファイルのパス
+     * @param  {Option} option { outputJsonPath:'', delimitter:':', pretty:true }
+     *       debug          : コンソールを出力します。
+     *       outputJsonPath : 出力するJsonのパス。空文字や文字列でない場合は出力しない。
+     *       delimitter     : CSVの区切り文字。１文字以上なければならない
+     *       pretty         : Jsonの出力形式。trueをいれると圧縮された状態で出力。
+     * @return {Promise} Promiseオブジェクト
+     */
+    loadCSV: function ( input ) {
+      var promise = new Promise( function ( resolve, reject ) {
+        app._dumpCsv( input )
+          .then( function ( csvArray ) {
+            createJson = app.convert( csvArray[0] )
 
-  if( _.isObject( option ) ) {
-    // debug
-    if( option.debug ) {
-      debug = true;
-    } else {
-      debug = false
-    }
-
-    // pretty
-    if( option.pretty ) {
-      pretty = true;
-    } else {
-      pretty = false
-    }
-
-    // outputJsonPath
-    if( _.isString( option.outputJsonPath ) ) {
-      outputJsonPath = option.outputJsonPath;
-    }
-
-    // delimitter
-    if( _.isObject( option ) && typeof _.isString( option.delimitter ) && option.delimitter !== '' ) {
-      delimitter = option.delimitter;
-    }
-  }
-
-  // 区切り文字
-  if( _.isUndefined( delimitter ) ) {
-    delimitter = ':';
-  }
-
-  reg = RegExp( delimitter );
-
-
-
-  /**
-   * main
-   * メインプロセス
-   * @return {Object} Promiseオブジェクト
-   */
-  function main() {
-
-    return new Promise( function( resolve, reject ){
-
-      _dumpCsv( input )
-        .then( function ( data ) {
-          var dataValue = data[0];
-          var dataName = dataValue.shift();
-      
-          _.forEach( dataName, function ( value, key, object ) {
-            if( reg.test( value ) ) {
-              dataName[ key ] = value.split( delimitter ).reverse();
+            if( _.isString( app.outputJsonPath ) && app.outputJsonPath !== '' ) {
+              var strJson;
+              if( app.pretty ) {
+                strJson = JSON.stringify( createJson, null, '    ' );
+              } else {
+                strJson = JSON.stringify( createJson );
+              }
+              fs.writeFile( app.outputJsonPath, strJson);
             }
+            resolve( createJson );
           });
-      
-          var createJson =  _.map( dataValue, function ( values ) {
-            var newData = {};
-      
-            _.forEach( values, function ( value, key, object ) {
-              var hash = dataName[key];
-              if( _.isArray( hash ) ) {
-                  newData = _.merge( newData, _deepProperty( hash, value ) )
-                } else {
-                  newData[ dataName[key] ] = value;
-                }
-              });
-              if( debug ) console.log( newData );
-              return newData;
-            });
-      
-          var str;
-          if( !pretty ) {
-            str = JSON.stringify( createJson );
+        })
+      return promise;
+    },
+
+    /**
+     * Convert an array of CSV format to the object of Json format.
+     *
+     * @param  {Arary} csvArray csv形式を維持した、1行目にプロパティ名を持つ配列
+     * @return {String} 出力したデータ内容
+     */
+    convert: function ( csvArray ) {
+      var dataValue = csvArray;
+      var dataName = dataValue.shift();
+
+      _.forEach( dataName, function ( value, key, object ) {
+        if( app.reg.test( value ) ) {
+          dataName[ key ] = value.split( app.delimitter ).reverse();
+        }
+      });
+
+      var createJson =  _.map( dataValue, function ( values ) {
+        var jsonFormatObject = {};
+
+        _.forEach( values, function ( value, key, object ) {
+          var hash = dataName[key];
+          if( _.isArray( hash ) ) {
+            jsonFormatObject = _.merge( jsonFormatObject, app._deepProperty( hash, value ) )
           } else {
-            str = JSON.stringify( createJson, null, '    ' );
+            jsonFormatObject[ dataName[key] ] = value;
           }
-
-          if( _.isString( outputJsonPath ) && outputJsonPath !== '' ) {
-            fs.writeFile( outputJsonPath, str );
-          }
-
-          resolve( createJson );
         });
-    });
-  }
-  
+        if( this.debug ) console.log( jsonFormatObject );
+        return jsonFormatObject;
+      });
 
-  /**
-   * _deepProperty
-   *
-   * @param {String} path 読み込むcsvのパス
-   * @return {Promise} Promiseオブジェクト
-   *
-   * @example
-   * _deepProperty( [ 'a','b','c' ], "mogeta" )
-   * output: { a: { b: { c: "mogeta" } } }
-   */
-  function _dumpCsv( path ) {
-  
-    var promise = new Promise( function ( resolve, reject ) {
-      var json_array = [];
-  
-      fs.readFile( path, function(err, sjisBuf) {
-        var buf = conv.convert(sjisBuf);
-        if( debug ) console.log( 'CSV Loading:' + path + ' *=-');
-        csv.parse(buf.toString(),{comment:'#'}, function(err, data) {
-          json_array.push( data );
-          resolve( json_array );
+      return createJson;
+    },
+
+
+    /**
+     * _dumpCsv
+     *
+     * @param {String} path 読み込むcsvのパス
+     * @return {Promise} Promiseオブジェクト
+     *
+     * @example
+     * _deepProperty( [ 'a','b','c' ], "mogeta" )
+     * output: { a: { b: { c: "mogeta" } } }
+     */
+    _dumpCsv: function ( path ) {
+      var promise = new Promise( function ( resolve, reject ) {
+        var json_array = [];
+   
+        fs.readFile( path, function(err, sjisBuf) {
+          var buf = conv.convert(sjisBuf);
+          if( app.debug ) console.log( 'CSV Loading:' + path + ' *=-');
+          csv.parse(buf.toString(),{comment:'#'}, function(err, data) {
+            json_array.push( data );
+            resolve( json_array );
+          });
         });
       });
-    })
-  
-    return promise;
+    
+      return promise;
+    },
+
+    /**
+     * _deepProperty
+     *
+     * @param {Array} hash 深い階層のハッシュを作るための配列
+     * @return {String} hashを元に作ったオブジェクト
+     *
+     * @example
+     * _deepProperty( [ 'a','b','c' ], "mogeta" )
+     * output: { a: { b: { c: "mogeta" } } }
+     */
+     _deepProperty: function ( hash, newValue ) {
+       var result;
+       _.forEach( hash, function ( value, key, object ) {
+         if( typeof result === 'undefined' ) {
+           result = {};
+           result[value] = newValue
+         } else {
+           resultChild = result;
+           result = {};
+           result[ value ] = resultChild;
+         }
+       });
+       return result;
+    }
   }
 
-  /**
-   * _deepProperty
-   *
-   * @param {Array} hash 深い階層のハッシュを作るための配列
-   * @return {String} hashを元に作ったオブジェクト
-   *
-   * @example
-   * _deepProperty( [ 'a','b','c' ], "mogeta" )
-   * output: { a: { b: { c: "mogeta" } } }
-   */
-  function _deepProperty( hash, newValue ) {
-    var result;
-    _.forEach( hash, function ( value, key, object ) {
-      if( typeof result === 'undefined' ) {
-        result = {};
-        result[value] = newValue
-      } else {
-        resultChild = result;
-        result = {};
-        result[ value ] = resultChild;
-      }
-    });
-    return result;
-  }
-
-  return main();
+  return app;
 }
 
+module.exports = csv2json;
--- test/main.js
+++ test/main.js
@@ -1,84 +1,108 @@
 var assert   = require('assert');
 var fs = require('fs');
-var csv2json = require('../csv2json.js');
+var csv2json = require('../csv2json.js')();
 var input = '',
     output = '';
 
-// パラメータ系テスト
-describe('パラメータ', function() {
-    it('CSVファイルが正しく読み込める', function() {
-      return csv2json( './test/csv/parameter1.csv', './test/json/parameter1.json' )
-        .then( function ( data ) {
-          assert( true );
-        })
-    });
-    it('区切り文字が"|"に変更されてを指定して正しく動作する', function() {
-      return csv2json( './test/csv/parameter2.csv', './test/json/parameter2.json', { delimitter:'|' } )
-        .then( function ( data ) {
-          assert( true );
-        });
-    });
-});
-
-
-// 文字コード系テスト（簡易）
-describe('違う文字コードでも正しく動作するか', function() {
-  it('UTF-8', function( done ) {
-    input = './test/csv/fileformat1.csv';
-    output = './test/json/fileformat1.json';
-    csv2json( input, { outputJsonPath: output } )
-      .then( function ( data ) {
-        assert( data[0].user   === 'sv.junic1' );
-        assert( data[0].field1 === 'field1-1Value' );
-        assert( data[0].field2.field2Child1 === 'field2-1Value' );
-        assert( data[0].field2.field2Child2 === 'field2-2Value' );
-        assert( data[0].field3.field3Child.field3ChildChild === '100' );
-        filecheck( output, function(){ done(); } )
+describe('loadCSVメソッドのテスト', function() {
+  // パラメータ系テスト
+  describe('パラメータ', function() {
+      it('CSVファイルが正しく読み込める', function() {
+        csv2json.setup( { outputJsonPath: './test/json/parameter1.json' } );
+        return csv2json.loadCSV( './test/csv/parameter1.csv', './test/json/parameter1.json' )
+          .then( function ( data ) {
+            assert( true );
+          })
+      });
+      it('区切り文字が"|"に変更されてを指定して正しく動作する', function() {
+        csv2json.setup( { outputJsonPath: './test/json/parameter2.json', delimitter:'|' } );
+        return csv2json.loadCSV( './test/csv/parameter2.csv' )
+          .then( function ( data ) {
+            assert( true );
+          });
       });
   });
-  it('Shift-JIS', function( done ) {
-    input = './test/csv/fileformat2.csv';
-    output = './test/json/fileformat2.json';
-    csv2json( input, { outputJsonPath: output } )
-      .then( function ( data ) {
-        assert( data[0].user   === 'sv.junic1' );
-        assert( data[0].field1 === 'field1-1Value' );
-        assert( data[0].field2.field2Child1 === 'field2-1Value' );
-        assert( data[0].field2.field2Child2 === 'field2-2Value' );
-        assert( data[0].field3.field3Child.field3ChildChild === '100' );
-        filecheck( output, function(){ done(); } )
-      })
+  
+  
+  // 文字コード系テスト（簡易）
+  describe('違う文字コードでも正しく動作するか', function() {
+    it('UTF-8', function( done ) {
+      input = './test/csv/fileformat1.csv';
+      output = './test/json/fileformat1.json';
+      csv2json.setup( { outputJsonPath: output } );
+      csv2json.loadCSV( input )
+        .then( function ( data ) {
+          assert( data[0].user   === 'sv.junic1' );
+          assert( data[0].field1 === 'field1-1Value' );
+          assert( data[0].field2.field2Child1 === 'field2-1Value' );
+          assert( data[0].field2.field2Child2 === 'field2-2Value' );
+          assert( data[0].field3.field3Child.field3ChildChild === '100' );
+          filecheck( output, function(){ done(); } )
+        });
+    });
+    it('Shift-JIS', function( done ) {
+      input = './test/csv/fileformat2.csv';
+      output = './test/json/fileformat2.json';
+      csv2json.setup( { outputJsonPath: output } );
+      csv2json.loadCSV( input )
+        .then( function ( data ) {
+          assert( data[0].user   === 'sv.junic1' );
+          assert( data[0].field1 === 'field1-1Value' );
+          assert( data[0].field2.field2Child1 === 'field2-1Value' );
+          assert( data[0].field2.field2Child2 === 'field2-2Value' );
+          assert( data[0].field3.field3Child.field3ChildChild === '100' );
+          filecheck( output, function(){ done(); } )
+        })
+    });
+  });
+   
+  
+  // 出力内容テスト
+  describe('出力内容テスト', function() {
+    it('区切り文字が";"で正しく出力される', function( done ) {
+      input = './test/csv/delimitter1.csv';
+      output = './test/json/delimitter1.json';
+      csv2json.setup( { outputJsonPath:output, delimitter:':' } );
+      csv2json.loadCSV( input )
+        .then( function ( data ) {
+          assert( data[0].user   === 'sv.junic1' );
+          assert( data[0].field1 === 'field1-1Value' );
+          assert( data[0].field2.field2Child1 === 'field2-1Value' );
+          assert( data[0].field2.field2Child2 === 'field2-2Value' );
+          assert( data[0].field3.field3Child.field3ChildChild === '100' );
+          filecheck( output, function(){ done(); } )
+        })
+    });
+    it('区切り文字が"|"で正しく出力される', function( done ) {
+      input = './test/csv/delimitter2.csv';
+      output = './test/json/delimitter2.json';
+      csv2json.setup( { outputJsonPath:output, delimitter:'|' } );
+      csv2json.loadCSV( input )
+        .then( function ( data ) {
+          assert( data[0].user   === 'sv.junic1' );
+          assert( data[0].field1 === 'field1-1Value' );
+          assert( data[0].field2.field2Child1 === 'field2-1Value' );
+          assert( data[0].field2.field2Child2 === 'field2-2Value' );
+          assert( data[0].field3.field3Child.field3ChildChild === '100' );
+          filecheck( output, function(){ done(); } )
+        })
+    });
   });
 });
  
+ 
 
 // 出力内容テスト
-describe('出力内容テスト', function() {
-  it('区切り文字が";"で正しく出力される', function( done ) {
-    input = './test/csv/delimitter1.csv';
-    output = './test/json/delimitter1.json';
-    csv2json( input, { outputJsonPath:output, delimitter:':' } )
-      .then( function ( data ) {
-        assert( data[0].user   === 'sv.junic1' );
-        assert( data[0].field1 === 'field1-1Value' );
-        assert( data[0].field2.field2Child1 === 'field2-1Value' );
-        assert( data[0].field2.field2Child2 === 'field2-2Value' );
-        assert( data[0].field3.field3Child.field3ChildChild === '100' );
-        filecheck( output, function(){ done(); } )
-      })
-  });
-  it('区切り文字が"|"で正しく出力される', function( done ) {
-    input = './test/csv/delimitter2.csv';
-    output = './test/json/delimitter2.json';
-    csv2json( input, { outputJsonPath:output, delimitter:'|' } )
-      .then( function ( data ) {
-        assert( data[0].user   === 'sv.junic1' );
-        assert( data[0].field1 === 'field1-1Value' );
-        assert( data[0].field2.field2Child1 === 'field2-1Value' );
-        assert( data[0].field2.field2Child2 === 'field2-2Value' );
-        assert( data[0].field3.field3Child.field3ChildChild === '100' );
-        filecheck( output, function(){ done(); } )
-      })
+describe('convertメソッドのテスト', function() {
+
+  var testdata = [
+    'user,field1,field2:field2Child1,field2:field2Child2,field3:field3Child:field3ChildChild',
+    'sv.junic1,field1-1Value,field2-1Value,field2-2Value,100'
+  ];
+
+  it('CSVの配列をJsonに変換する', function() {
+    csv2json.setup( { debug:true, pretty:true } );
+    csv2json.convert( testdata );
   });
 });
  
@@ -87,12 +111,15 @@ describe('出力内容テスト', function() {
 // 出力内容テスト
 describe('デバッグ用動作テスト', function() {
   it('Jsonが非圧縮状態で出力される', function( done ) {
-    csv2json( './test/csv/debug.csv', './test/json/debug.json', { debug:true, pretty:true } )
+    csv2json.setup( { outputJsonPath: './test/json/debug.json', debug:true, pretty:true } );
+    csv2json.loadCSV( './test/csv/debug.csv' )
       .then( function ( data ) {
         done();
       })
   });
 });
+
+
 
 function filecheck( filepath, callback ) {
   fs.open( filepath, 'r', 384 /*=0600*/, function(err, fd) {
